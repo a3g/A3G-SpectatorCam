a9445e18b947a0a567f10c4f300e9c2628798216
Cephei <cepheidev+github@gmail.com>
2015-03-13 04:34:18 +0100

    Added a3g framework compability
    Changed required path from a3g-spectator-cam to
    modules\a3g-spectator-cam

---
+++ module.hpp
@@ -0,0 +1,19 @@
+#ifdef A3G_Framework_state_Module
+class A3G_SpectatorCam {
+  name = "A3G Spectator Cam";
+  description = "A simple spectator cam script made for AGM users. Compatible with ACRE and TFAR.";
+  authors[] = {"TrustyArabian", "Cephei"};
+};
+#endif
+
+#ifdef A3G_Framework_state_Functions
+  #include "cfgFunctions.hpp"
+#endif
+
+#ifdef A3G_Framework_state_RespawnTemplates
+  #include "cfgRespawnTemplates.hpp"
+#endif
+
+#ifdef A3G_Framework_state_RscTitles
+  #include "rscTitles.hpp"
+#endif
